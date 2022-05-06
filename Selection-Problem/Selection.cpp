c65c83a675ec8d759211ff97c571de165d1c083c
talym <talym@mta.ac.il>
2022-05-06 10:09:07 +0300

    fix bug of selection

--- Selection-Problem/Selection.cpp
+++ Selection-Problem/Selection.cpp
@@ -16,14 +16,12 @@ Person* select(vector<Person>& personArr, int left, int right, int k, int& numCo
 	int pivotIndex;
 	int leftPart;
 
-	randIndex = rand() % personArr.size();
+	randIndex = left + (rand() % (right - left + 1));
 	pivotIndex = partition(personArr, left, right, randIndex, numComp);
 	leftPart = pivotIndex - left + 1;
-	numComp++;
 	if (k == leftPart){
 		return &personArr[pivotIndex];
 	}
-	numComp++;
 	if (k < leftPart) {
 		return select(personArr, left, pivotIndex - 1, k, numComp);
 	}
@@ -42,17 +40,29 @@ int partition(vector<Person>& personArr, int left, int right, int randIndex, int
 	swap(personArr[left], personArr[randIndex]);
 
 	while (pivot != nonPivot){
-		numComp++;
-		if (personArr[pivot].getPersonId() > personArr[nonPivot].getPersonId()){
-			swap(personArr[pivot], personArr[nonPivot]);
-			swap(pivot, nonPivot);
-			nonPivotIsOnRight = !nonPivotIsOnRight;
-		}
 		if (nonPivotIsOnRight){
-			nonPivot--;
+			numComp++;
+			if (personArr[pivot].getPersonId() > personArr[nonPivot].getPersonId()) {
+				swap(personArr[pivot], personArr[nonPivot]);
+				swap(pivot, nonPivot);
+				nonPivotIsOnRight = !nonPivotIsOnRight;
+				nonPivot++;
+			}
+			else {
+				nonPivot--;
+			}
 		}
 		else {
-			nonPivot++;
+			numComp++;
+			if (personArr[nonPivot].getPersonId() > personArr[pivot].getPersonId()) {
+				swap(personArr[nonPivot], personArr[pivot]);
+				swap(nonPivot, pivot);
+				nonPivotIsOnRight = !nonPivotIsOnRight;
+				nonPivot--;
+			}
+			else {
+				nonPivot++;
+			}
 		}
 	}
 
