893763095f092efd724b212612cd7fd33123a3a0
talym <talym@mta.ac.il>
2022-05-12 16:05:24 +0300

    update

--- Selection-Problem/Selection.h
+++ Selection-Problem/Selection.h
@@ -4,7 +4,7 @@
 #include "vector"
 
 
-//selection functions
+
 const Person& randSelection(vector<Person>& personArr, int k, int& numComp);
 Person* select(vector<Person>& personArr, int left, int right, int k, int& numComp);
 int partition(vector<Person>& personArr, int left, int right, int randIndex, int& numComp);
